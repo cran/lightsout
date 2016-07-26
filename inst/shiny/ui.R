b05b5b6a2641f6cd90b1b0f53739c20a3be5e85d
Dean Attali <daattali@gmail.com>
2016-07-26 12:09:35 +0000

    version 0.3

--- inst/shiny/ui.R
+++ inst/shiny/ui.R
@@ -1,14 +1,50 @@
 source("global.R")
 
+share <- list(
+  title = "Lights Out",
+  url = "http://daattali.com/shiny/lightsout/",
+  image = "http://daattali.com/shiny/img/lightsout.png",
+  description = "Play the classic Lights Out puzzle game in R",
+  twitter_user = "daattali"
+)
+
 fluidPage(
   title = "Lights Out / Dean Attali",
   shinyjs::useShinyjs(),
   tags$head(
-    tags$link(href = "style.css", rel = "stylesheet")
+    tags$link(href = "style.css", rel = "stylesheet"),
+
+    # Favicon
+    tags$link(rel = "shortcut icon", type="image/x-icon", href="favicon.ico"),
+
+    # Facebook OpenGraph tags
+    tags$meta(property = "og:title", content = share$title),
+    tags$meta(property = "og:type", content = "website"),
+    tags$meta(property = "og:url", content = share$url),
+    tags$meta(property = "og:image", content = share$image),
+    tags$meta(property = "og:description", content = share$description),
+
+    # Twitter summary cards
+    tags$meta(name = "twitter:card", content = "summary"),
+    tags$meta(name = "twitter:site", content = paste0("@", share$twitter_user)),
+    tags$meta(name = "twitter:creator", content = paste0("@", share$twitter_user)),
+    tags$meta(name = "twitter:title", content = share$title),
+    tags$meta(name = "twitter:description", content = share$description),
+    tags$meta(name = "twitter:image", content = share$image)
   ),
-  div(
-    id = "titlePanel",
-    "Lights Out"
+  div(id = "header",
+      div(id = "title",
+          "Lights Out"
+      ),
+      div(id = "subsubtitle",
+          "Created by",
+          tags$a(href = "http://deanattali.com/", "Dean Attali"),
+          HTML("&bull;"),
+          "Package available",
+          tags$a(href = "https://github.com/daattali/lightsout", "on GitHub"),
+          HTML("&bull;"),
+          tags$a(href = "http://daattali.com/shiny/", "More apps"), "by Dean"
+      )
   ),
   fluidRow(
     column(4, wellPanel(
@@ -24,13 +60,7 @@ fluidPage(
       p("Lights Out is a puzzle game consisting of a grid of lights that",
       "are either", em("on"), "(light green) or", em("off"), "(dark green). In", em("classic"), "mode, pressing any light will toggle",
       "it and its adjacent lights. In", em("variant"), "mode, pressing a light will toggle all the lights in its",
-      "row and column. The goal of the game is to", strong("switch all the lights off.")),
-      span(
-        id = "created-by-dean",
-        "Created by", a("Dean Attali", href = "http://deanattali.com"),
-        HTML("&bull;"), "Code",
-        a("on GitHub", href = "https://github.com/daattali/lightsout")
-      )
+      "row and column. The goal of the game is to", strong("switch all the lights off."))
     )),
     column(8,
       hidden(div(id = "congrats", "Good job, you won!")),
